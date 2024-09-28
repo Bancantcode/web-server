4495c64060fa594ce247fa9f06dc5174382725c6
Bancantcode <santiagobryan831@gmail.com>
2024-09-28 13:44:22 +0800

    resume using js

---
+++ ResumeUsingEJS/SantiagoBry_303/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { 
+        name: "Bryan Aaron B. Santiago",
+        email: "santiagobryan831@gmail.com",
+        number: "09282977154",
+        github: "github.com/Bancantcode",
+        school: "Holy Angel University",
+        schoolAddress: "#1 Holy Angel St, Angeles, 2009 Pampanga",
+        course: "B.S in Information Technology with area specialization in Web Development",
+        schoolYear: "July 2022-2026",
+        honor: "Deans List",
+        gpa: "1.30",
+        section1: "Education",
+        section2: "Projects",
+        section3: "Certificates",
+        section4: "Technical Skills",
+        relevantCoursework: [
+            "Dynamic Web Application and Development Tools",
+            "Data Structure and Algorithms",
+            "Object-Oriented Programming",
+            "Information Assurance and Security",
+            "Advance Database",
+            "Web and Advertising Publishing Concepts",
+            "Web Server and Client Services"
+        ],
+        projects: [
+            { 
+                name: "Responsive CRUD Coffee Website", 
+                description: "Designed and implemented a fully functional CRUD website that ensures seamless user experience for placing and managing orders",
+                made: "Made using HTML, CSS, JAVASCRIPT, PHP, MYSQL"
+            },
+            {
+                name: "SOGA AI Art", 
+                description: "Created a website that showcase the beauty of AI generated photos",
+                made: "Made using NEXTJS, SASS, JAVASCRIPT, GSAP"
+            },
+            {
+                name: "Early Stage Venture Capital Fund", 
+                description: "An early stage venture capital fund that sources and invests in technology companies run by young innovators",
+                made: "Made using REACTJS, TAILWINDCSS, GSAP"
+            },
+            {
+                name: "Digital Clock, Stopwatch, To Do List", 
+                description: "Designed a user-friendly Digital Clock, Stopwatch and To Do List application for efficient task management",
+                made: "Made using React.js and CSS"
+            }
+        ],
+        certificates: [
+            "SEO Rank & Rise: The Beginner’s Blueprint to Web Winning (Code Geeks)",
+            "The Complete 2024 Web Development Bootcamp (Udemy)",
+            "Legacy Javascript Algorithms and Data Structures (freecodecamp)",
+            "Responsive Web Design (freecodecamp)",
+            "Get Started with Figma (Coursera)"
+        ],
+        skillset: [
+            "Javascript",
+            "HTML",
+            "CSS",
+            "SASS",
+            "TAILWINDCSS",
+            "JQUERY",
+            "REACT.JS",
+            "NEXT.JS",
+            "PYTHON",
+            "JAVA",
+            "PHP",
+            "GSAP",
+            "NODE.JS",
+            "EXPRESS.JS",
+            "MYSQL",
+            "MONGODB",
+            "FIGMA",
+            "Web Development",
+            "Wireframing",
+            "UI/UX Design",
+            "Branding"
+        ],
+    });
+});
+
+app.listen(3000, () => console.log('Listening on port http://localhost:3000'));
